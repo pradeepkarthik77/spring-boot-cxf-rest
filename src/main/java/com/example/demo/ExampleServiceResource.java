acbcf483f7e95dc39edea0d2026ddb79225d4bb0
pradeepkarthik77 <77573751+pradeepkarthik77@users.noreply.github.com>
2024-05-15 16:16:37 +0530

    init commit

--- src/main/java/com/example/demo/ExampleServiceResource.java
+++ src/main/java/com/example/demo/ExampleServiceResource.java
@@ -2,7 +2,12 @@ package com.example.demo;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -14,5 +19,13 @@ public class ExampleServiceResource {
     @Path("/service")
     public String getHelloWorld() {
         return "Hello World";
+    }
+    
+    @POST
+    @Path("/employees")
+    public String enterEmployee(@NotNull @HeaderParam("name") String employee) throws Exception
+    {
+    	throw new Exception();
+//    	return "okay";
     }
 }
